b1dfaa6f32ca88b6231ac2555447f45ad0f4084b
daksh31 <daksh7064.mca25@chitkara.edu.in>
2025-09-08 10:41:15 +0530

    Added <program2.java>

--- program2.java
+++ program2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class sum{
+public class program2{
     public static void main(String[] args){
         System.out.print("Enter a number:");
         Scanner sc = new Scanner(System.in);
